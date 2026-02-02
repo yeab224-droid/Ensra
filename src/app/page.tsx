36d78c50563c706c6ad4e7e561222ab8062ee7bd
yeab <yeab224@gmail.com>
2026-02-02 19:17:32 +0300

    setup finishing

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+const  X = () =>{
+  return (
+    <div >
+<Button variant="secondary">touch me sonnya</Button>
+    </div>
+  )
+}
+export default X;
